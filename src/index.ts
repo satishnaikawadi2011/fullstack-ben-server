61dedae463ac591d41da3eb8173c2e46c37be629
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2020-10-07 00:55:04 +0530

    completed hello query with apollo-server-express and type-graphql

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-// psql -U postgres
-
+//TODO: psql -U postgres
+import 'reflect-metadata';
+import { HelloResolver } from './resolvers/hello';
 import { Post } from './entities/Post';
 import { __prod__ } from './constants';
 import { MikroORM } from '@mikro-orm/core';
 import mikroConfig from './mikro-orm.config';
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
 
 const PORT = process.env.PORT || 4000;
 
@@ -12,9 +15,22 @@ const main = async () => {
 	const orm = await MikroORM.init(mikroConfig);
 	await orm.getMigrator().up();
 	const app = express();
-	app.get('/', (req, res) => {
-		res.send('Hello');
+	// app.get('/', (req, res) => {
+	// 	res.send('Hello');
+	// });
+	const apolloServer = new ApolloServer({
+		schema:
+			await buildSchema({
+				resolvers:
+					[
+						HelloResolver
+					],
+				validate: false
+			})
 	});
+
+	apolloServer.applyMiddleware({ app });
+
 	app.listen(PORT, () => {
 		console.log(`Server listening on port ${PORT}`);
 	});
