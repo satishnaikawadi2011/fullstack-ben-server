4ffa64b3f9d2a6e7f9ca5ca88efe5e953aa45ba0
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2020-10-07 10:14:42 +0530

    up and running redis server on windows

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
+import { MyContext } from './types';
 import { UserResolver } from './resolvers/user';
 import { PostResolver } from './resolvers/post';
-//TODO: psql -U postgres
+
+//TODO: using psql
+//  psql -U postgres
+
+// TODO: redis server run on windows
+// C:\redis\redis-server.exe C:\redis\redis.windows.conf
+
 import 'reflect-metadata';
 import { HelloResolver } from './resolvers/hello';
 import { Post } from './entities/Post';
@@ -11,8 +18,11 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 
-const PORT = process.env.PORT || 4000;
+import redis from 'redis';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
 
+const PORT = process.env.PORT || 4000;
 const main = async () => {
 	const orm = await MikroORM.init(mikroConfig);
 	await orm.getMigrator().up();
@@ -20,6 +30,31 @@ const main = async () => {
 	// app.get('/', (req, res) => {
 	// 	res.send('Hello');
 	// });
+
+	let RedisStore = connectRedis(session);
+	let redisClient = redis.createClient();
+
+	app.use(
+		session({
+			name: 'qid',
+			store:
+				new RedisStore({
+					client: redisClient,
+					disableTouch: true
+					// disableTTL:true
+				}),
+			cookie:
+				{
+					maxAge: 1000 * 60 * 60 * 24 * 365 * 10, //10 years
+					httpOnly: true,
+					secure: __prod__, //cookie only works in https
+					sameSite: 'lax'
+				},
+			secret: 'this_is_my_secret',
+			resave: false
+		})
+	);
+
 	const apolloServer = new ApolloServer({
 		schema:
 			await buildSchema({
@@ -31,7 +66,7 @@ const main = async () => {
 					],
 				validate: false
 			}),
-		context: () => ({ em: orm.em })
+		context: ({ req, res }): MyContext => ({ em: orm.em, req, res })
 	});
 
 	apolloServer.applyMiddleware({ app });
